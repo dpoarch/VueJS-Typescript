5e1e2c37b6af43e8708fae64aa78bfdbaf01ee59
dpoarch <davidpoarch@outlook.com>
2022-04-20 22:20:43 +0800

    initial commit

---
+++ src/request.ts
@@ -0,0 +1,15 @@
+export type User = { name: string; username: string }
+export type UserList = Array<User & { password: string }>
+
+export async function login(username: string, password: string){
+    const request = await fetch('/users.json');
+    const userJson: UserList = await request.json();
+    return userJson.find(s => s.password === password && s.username === username) 
+}
+
+
+export async function getUser(){
+    const request = await fetch('/current.json');
+    const user: User = await request.json()
+    return user
+}
